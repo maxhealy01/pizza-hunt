30a4b341d2a50940a007e025690addcc0b72dccd
Max Healy <maxhealy01@gmail.com>
2021-02-06 17:10:46 -0600

    Created Pizza routes in MongoDB

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const {
+	getAllPizza,
+	getPizzaById,
+	createPizza,
+	updatePizza,
+	deletePizza,
+} = require("../../controllers/pizza-controller");
+
+// set up GET all and POST at /api/pizzas
+router.route("/").get(getAllPizza).post(createPizza);
+
+router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
+
+module.exports = router;
